f2d304daaa62d309a76256c48d37872b6806d8a3
Ezequiel <ezequiel1de1oliveira@gmail.com>
2019-12-18 09:19:47 -0300

    theme changed
    Signed-off-by: Ezequiel <ezequiel1de1oliveira@gmail.com>

--- quasar.conf.js
+++ quasar.conf.js
@@ -93,7 +93,7 @@ module.exports = function (ctx) {
         display: 'standalone',
         orientation: 'portrait',
         background_color: '#ffffff',
-        theme_color: '#027be3',
+        theme_color: '#ffffff',
         icons: [
           {
             'src': 'statics/icons/icon-128x128.png',
