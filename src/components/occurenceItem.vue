0d67cbe2823eca6665383e15c3e4b54429d37f38
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2020-01-27 16:45:47 -0300

    Add basic connection to occurences endpoint
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>

--- src/components/occurenceItem.vue
+++ src/components/occurenceItem.vue
@@ -3,12 +3,12 @@
     <div v-for="item in items" v-bind:key="item.id">
       <q-item to="/transducer">
         <q-item-section>
-          <q-item-label>{{item.name}} ({{item.sigla}})</q-item-label>
+          <q-item-label>{{item.location}} ({{item.campus}})</q-item-label>
           <q-item-label caption>{{item.obs}}</q-item-label>
         </q-item-section>
 
         <q-item-section side top>
-          <q-item-label caption>{{item.time}}</q-item-label>
+          <q-item-label caption>{{writenTime(item.time)}}</q-item-label>
         </q-item-section>
       </q-item>
       <q-separator spaced inset />
@@ -26,6 +26,21 @@ export default {
       default: []
     }
     /* eslint-enable */
+  },
+  methods: {
+    writenTime(min){
+      if(min < 60){
+        return min + " min";
+      }else{
+        let h = Math.floor(min / 60);
+        let m = min % 60;
+        if(m == 0){
+          return h + " horas";
+        }else{
+          return h + "h " + m + "min";
+        }
+      }
+    }
   }
 }
 </script>
