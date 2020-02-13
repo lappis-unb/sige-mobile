d50cf8135709a7214f46c589b194e69a977cad2e
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2020-02-13 15:13:26 -0300

    Add icon on transducers with occurences
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>

--- src/components/transducerList.vue
+++ src/components/transducerList.vue
@@ -26,10 +26,19 @@ export default {
     return {
       transducers: [],
       campi: [],
+      occurences: [],
       loading: true
     }
   },
   async created () {
+    await MASTER.get('/occurences/')
+      .then((res) => {
+        this.occurences = res.data
+      })
+      .catch((err) => {
+        console.log('ERROR: ', err)
+      })
+
     await MASTER.get('/campi/')
       .then((res) => {
         this.campi = res.data
@@ -40,9 +49,8 @@ export default {
 
     await MASTER.get('/energy-transductors/')
       .then((res) => {
-        console.log('Transducer list:', res.data)
-        res.data.forEach((i) => {
-          this.transducers.push(this.addCampus(i))
+        res.data.forEach(async (i) => {
+          this.transducers.push(await this.addInfos(i))
         })
       })
       .catch((err) => {
@@ -51,20 +59,53 @@ export default {
     this.loading = false
   },
   methods: {
-    addCampus (item, campi) {
+    async addInfos (item) {
       let id = parseInt(item.campus.split('/')[4], 10)
+      let occ = await this.verfyOccurences(item)
+
       return {
         ...item,
         campus_id: id,
-        campus_acronym: this.campi.find((x) => x.id === id).acronym
+        campus_acronym: this.campi.find((x) => x.id === id).acronym,
+        light_occurence: occ.light,
+        serious_occurence: occ.serious
       }
+    },
+
+    async verfyOccurences (transducer) {
+      let id = transducer.serial_number
+      let occ = {
+        serious: false,
+        light: false
+      }
+      if (await this.checkArr(this.occurences.transductor_connection_fail, id, occ.light) ||
+          await this.checkArr(this.occurences.slave_connection_fail, id, occ.light) ||
+          await this.checkArr(this.occurences.precarious_tension, id, occ.light)) {
+        occ.light = true
+      }
+
+      if (await this.checkArr(this.occurences.critical_tension, id, occ.serious) ||
+          await this.checkArr(this.occurences.phase_drop, id, occ.serious)) {
+        occ.serious = true
+      }
+
+      return occ
+    },
+
+    checkArr (arr, id, occ) {
+      let ans = false
+      arr.forEach((elem) => {
+        if (elem.transductor === id) {
+          ans = true
+        }
+      })
+      return ans
     },
 
     filterTransducers () {
       let arr = this.transducers
       let campus = this.$store.state.campus
       let transducers = []
-      console.log('AKI ==>', arr)
       arr.forEach((elem) => {
         if (campus.id === null || elem.campus_id === campus.id) {
           transducers.push(elem)
