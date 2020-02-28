8e002f95ba7599c1c509b2783ec91f3358600d31
Ezequiel <ezequiel1de1oliveira@gmail.com>
2020-02-28 20:09:14 -0300

    Added axios service for firebase
    Signed-off-by: Ezequiel <ezequiel1de1oliveira@gmail.com>

---
+++ src/services/firebaseAPI/http-common.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+const FIREBASEAPI = axios.create({
+  baseURL: 'https://iid.googleapis.com/iid/v1/',
+  headers: { 'Authorization': `key=${process.env.KEY_SERVER}`}
+})
+
+export default FIREBASEAPI
