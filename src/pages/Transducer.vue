4ccfe23aff9b9233e3db6a324b10782fc455c0b3
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2020-03-20 19:15:36 -0300

    Fix bug on last reading time
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>

--- src/pages/Transducer.vue
+++ src/pages/Transducer.vue
@@ -151,7 +151,8 @@ export default {
           r: Math.round(res.data[0].total_reactive_power),
           t: Math.round(res.data[0].total_power_factor)
         }
-        this.lastReading = this.getTime(res.data.collection_time)
+
+        this.lastReading = this.getTime(res.data[0].collection_date)
         this.hasMeasurements = true
       })
       .catch(err => {
